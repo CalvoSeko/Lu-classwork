eb3dc56b4b867b776653f4411768309c157a82fd
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2021-09-17 22:30:57 +0200

    Actual version 2 of thief!
    Last commit was bugged.  Tony

--- pin/simple_pin.py
+++ pin/simple_pin.py
@@ -1,10 +1,12 @@
-x=str(input())
-listStr=[]
-tempList=[]
-count=0
-combinations=[]
-for i in range(4):
-    list.append(x[count])
-    count=count+1
-    for i in range(1,4):
-        tempList.append(listStr[count])
+pin=str(input("insert 4 digit pin "))
+list = [] 
+count = 0
+for i in range(0,4): 
+    for j in range(0,4): 
+        for k in range(0,4): 
+            for l in range(0,4):
+            	if i != j and i != k and i != l and i != l and j != k and j != l and k != l:
+                	count += 1
+                	list.append(1000*int(pin[i])+100*int(pin[j])+10*int(pin[k])+int(pin[l]))
+print(count)
+print(list)
