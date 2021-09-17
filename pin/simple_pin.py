7453362e886b2f4cb4b84c144639e555fd30c8e5
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2021-09-17 10:42:49 +0200

    Thief!
    First class

---
+++ pin/simple_pin.py
@@ -0,0 +1,10 @@
+x=str(input())
+listStr=[]
+tempList=[]
+count=0
+combinations=[]
+for i in range(4):
+    list.append(x[count])
+    count=count+1
+    for i in range(1,4):
+        tempList.append(listStr[count])
