3cff3965ee4d25b9e3a76a38e3a491ad6b3705e7
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-10-04 15:07:00 +0100

    Feedback: Rock Paper Scissors
    This is very good and great that you managed without if statements. Try again, but using mod...

--- challenges/rockPaperScissorsGame.py
+++ challenges/rockPaperScissorsGame.py
@@ -1,17 +1,18 @@
 import random
-rounds=int(input("How many rounds do you want to go? "))
+rounds=int(input("How many rounds do you want to go? ")) ### SRC - Okay, but I wanted an Yes/No answer...
 count=0
-while count!=rounds:
+while count!=rounds: ### SRC - If you know how many rounds there are use a for loop.
     print("0 rock, 1 paper, 2 scissors")
     print("guess:", end=" ")
     guess = int(input())
     print("guessed ", guess)
     computer_guess = random.randint(0,2)
     print("computer guessed", computer_guess)
-    situations = [[0,2,1], [1,0,2], [2,1,0], [3,3,3]]
+    situations = [[0,2,1], [1,0,2], [2,1,0], [3,3,3]] ### SRC - This is good, but there's an easier way with MOD
     index = situations[guess][computer_guess]
     outputs = ["tie", "You win", "the computer wins"]
     result = outputs[index]
     count+=1
     print(result)
     print()
+#end while
